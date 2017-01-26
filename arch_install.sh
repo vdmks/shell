a64f35a180b02639c197886793d47e29d6ee3e4b
Vadim <vdmanatolyevich@gmail.com>
2017-01-26 23:23:17 +0700

    Update arch_install.sh
    .

--- arch_install.sh
+++ arch_install.sh
@@ -5,8 +5,8 @@ ischroot=0
 if [ $ischroot -eq 0 ]
 then
 	loadkeys ru
-	(echo o; echo n; echo p; echo 1; echo 200; echo w) | fdisk /dev/sdc
-	(echo o; echo n; echo 4000; echo w) | fdisk /dev/sdc
-	(echo o; echo n; echo 3000; echo w) | fdisk /dev/sdc
+	(echo o; echo n; echo p; echo 1; echo 200; echo w) | fdisk /dev/sda
+	(echo o; echo n; echo 4000; echo w) | fdisk /dev/sda
+	(echo o; echo n; echo 3700; echo w) | fdisk /dev/sda
 	echo "Done"
 fi
